79d57e4b63c7cd0a31b6ff87f3f37e24dcb84d50
salman9633 <salmanvaipees913@gmail.com>
2025-02-09 20:24:56 +0300

    const variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
+    /* let */
+
+    //let are block-scoped, meaning they are only accessible within the block they are
     //by default, variables are immutable in rust
     let mut x = 5; // if we assign like let x=5 then you can reassign 6 to x next time
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    /* const */
+    const THREE_HOUR: u32 = 60 * 3; //const variables are always immutable
+                                    //Constants are globally scoped and can be accessed from anywhere in the program.
+    println!("Three Hours: {}", THREE_HOUR);
 }
