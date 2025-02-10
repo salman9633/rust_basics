319b0107a6567c08c038c52658254caab723e28a
salman9633 <salmanvaipees913@gmail.com>
2025-02-10 20:15:15 +0300

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,7 +10,24 @@ fn main() {
 
     /* const */
     const THREE_HOUR: u32 = 60 * 3; //const variables are always immutable
-    //Constants are globally scoped and can be accessed from anywhere in the program.
-    //for const the naming convention is UPPER_CASE (uppercase with underscore for seperation)
+                                    //Constants are globally scoped and can be accessed from anywhere in the program.
+                                    //for const the naming convention is UPPER_CASE (uppercase with underscore for seperation)
     println!("Three Hours: {}", THREE_HOUR);
+
+    fn shadowing() {
+        let x = 5;
+
+        let x = x + 1;
+
+        {
+            let x = x * 2;//hre value of x will be 12
+            println!("The value of x in the inner scope is: {x}");
+
+            //after ending this scope the x value will comes to 6 again
+        }
+
+        println!("The value of x is: {x}");
+    }
+
+    shadowing();
 }
