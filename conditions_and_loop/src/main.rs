26f472dad7bdc1aac2297d7e1d25173901634c29
salman9633 <salmanvaipees913@gmail.com>
2025-02-22 00:35:43 +0530

    loop

--- conditions_and_loop/src/main.rs
+++ conditions_and_loop/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     }else if !is_even{
         println!("The number is odd");
     }
+
+    //loops
+
+    for i in 0..10{// it will loop from 0 to 9
+        println!("The value of i is: {i}");
+    }
 }
