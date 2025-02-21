8f205faed18f7f569c5188acbea94f37a14c39b9
salman9633 <salmanvaipees913@gmail.com>
2025-02-22 00:50:09 +0530

    for loop example with string

--- conditions_and_loop/src/main.rs
+++ conditions_and_loop/src/main.rs
@@ -12,4 +12,23 @@ fn main() {
     for i in 0..10{// it will loop from 0 to 9
         println!("The value of i is: {i}");
     }
+
+    let sentence= String::from("My Name is Salman");
+
+    let first_word=find_the_first_word(sentence);
+    println!("The first word is: {first_word}");
+}
+
+fn find_the_first_word(sentence:String)->String//mentioning the return type
+{
+
+    let mut ans=String::new();
+
+    for chars in sentence.chars(){
+        ans.push_str(chars.to_string().as_str());
+        if chars==' '{
+            break;
+        }
+    }
+    return ans;
 }
