ba7404948395428318a1afe932b0c3512adaf9c7
salman9633 <salmanvaipees913@gmail.com>
2025-03-03 20:33:02 +0300

    mutable refferences

--- refference_and_borrowing/src/main.rs
+++ refference_and_borrowing/src/main.rs
@@ -12,8 +12,23 @@ fn main() {
     Here in this case we are calling or using s1 the even after it's reassigned to another variable.
     here actually we are ressigning with the '&' that means the refference of s1 is assigned to s2. same as we are passing the refference to the function
      */
+
+    /* mutable refference */
+
+    let mut a = String::from("Mutable Value A");
+    // let b = &mut a;
+    update_value(&mut a);
+
+    println!("The value of a is: {}", a);//out put ==>The value of a is: Mutable Value A Updated Value
+
+    // println!("The value of a is: {}", b);
 }
 
-fn calculate_length(s: &String) -> usize {//borrowing here
+fn calculate_length(s: &String) -> usize {
+    //borrowing here
     return s.len();
+}
+
+fn update_value(s: &mut String) {//borrow muttably
+    s.push_str(" Updated Value");
 }
