ff22c8a5fa6c7d70d41fe58249f5658a60024d9d
salman9633 <salmanvaipees913@gmail.com>
2025-03-03 19:37:47 +0300

    refference & borrowing

---
+++ refference_and_borrowing/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    /*
+    A reference is like a pointer in that it’s an address we can follow to access the data stored at that address; that data is owned by some other variable. Unlike a pointer, a reference is guaranteed to point to a valid value of a particular type for the life of that reference.
+     */
+
+    let s1 = String::from("hello");
+
+    let s2 = &s1;
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}{}.", s1, len, s2);
+    /*
+    Here in this case we are calling or using s1 the even after it's reassigned to another variable.
+    here actually we are ressigning with the '&' that means the refference of s1 is assigned to s2. same as we are passing the refference to the function
+     */
+}
+
+fn calculate_length(s: &String) -> usize {
+    return s.len();
+}
