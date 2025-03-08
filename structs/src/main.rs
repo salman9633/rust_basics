7c178507f3298dd8a09edd214edcf27e8e9380e8
salman9633 <salmanvaipees913@gmail.com>
2025-03-08 17:34:45 +0300

    implementation in struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,18 +4,40 @@ struct User {
     is_active: bool,
 }
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area_of_rectangle(&self) -> u32 {//this self variable have a access to the struct, just like we are using this keyword
+        self.width * self.height
+    }
+}
 fn main() {
     /*
     A struct, or structure, is a custom data type that lets you package together and name multiple related values that make up a meaningful group
 
     simply what object in javascript is a struct
     */
-    let age=23;
-    let user =User{
+    let age = 23;
+    let user = User {
         name: String::from("Salman"),
         age,
-        is_active: true
+        is_active: true,
     };
 
-    println!("The name of the user is: {} is {} old", user.name,user.age);
+    println!("The name of the user is: {} is {} old", user.name, user.age);
+
+    /*
+    You can also implement structs, which means you can attach functions to instaces of the struct.
+    very similar to classes in TS
+    */
+
+    let rect = Rectangle{
+        width: 50,
+        height: 20,
+    };
+    println!("The Area Of The Rectangle Is: {}", rect.area_of_rectangle())
+
 }
