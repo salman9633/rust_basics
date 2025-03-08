6c486821ebd2337a33de1d6d94d36ee4d8b67d00
salman9633 <salmanvaipees913@gmail.com>
2025-03-08 19:54:20 +0300

    basic set up of enum

---
+++ enums/src/main.rs
@@ -0,0 +1,19 @@
+enum Direction {
+    north,
+    south,
+    west,
+    east,
+
+}
+
+fn main() {
+    /*
+    Enums allows you to define a type by enumerating its possible variants
+    */
+
+    let my_direction = Direction::south;
+    current_direction(my_direction);
+}
+fn current_direction(direction: Direction) {
+//Login Here
+}
