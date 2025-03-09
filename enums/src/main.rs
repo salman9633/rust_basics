80e18753aa1687b9a02c055f939db6ec06b8e30c
salman9633 <salmanvaipees913@gmail.com>
2025-03-09 12:07:47 +0300

    pattern matching in enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,7 +3,12 @@ enum Direction {
     south,
     west,
     east,
+}
 
+enum Shape {
+    Circle(f32),
+    Square(f32),
+    rectangle(f32, f32),
 }
 
 fn main() {
@@ -13,7 +18,26 @@ fn main() {
 
     let my_direction = Direction::south;
     current_direction(my_direction);
+
+    let circle = Shape::Circle(5.0);
+    let rectangle = Shape::rectangle(5.5, 2.5);
+    let square = Shape::Square(3.5);
+
+    let area=calculate_area(square);
+    println!("Area: {}",area)
 }
 fn current_direction(direction: Direction) {
-//Login Here
+    //Login Here
+}
+
+fn calculate_area(shape: Shape) -> f32 {
+    let area = match shape {
+        Shape::Circle(radius) => 3.14 * radius * radius,
+        Shape::Square(side) => {
+            println!("Square");
+            side * side
+        }
+        Shape::rectangle(width, hiegth) => width * hiegth,
+    };
+    return area;
 }
