fddea396896827ca797e43785b2c150e2df91184
salman9633 <salmanvaipees913@gmail.com>
2025-06-29 13:46:42 +0300

    finding even numbers in a vector

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -10,7 +10,25 @@ fn vectors(){
     vec.push(1);
     vec.push(2);
     vec.push(3);
+    vec.push(4);
+    vec.push(5);
 
     println!("{:?}",vec);//you can directly print like we do in other datas
+    let even_vec=even_from_vect(vec);
+    println!("{:?}",even_vec);
+
     
+}
+
+fn even_from_vect(vec:Vec<i32>)->Vec<i32>{
+    let mut new_vec:Vec<i32>=Vec::new();
+
+    for val in vec{
+        if val%2==0{
+            println!("{}",val);
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec
 }
