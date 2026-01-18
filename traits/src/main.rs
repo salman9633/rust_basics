e51a918a0e1863cf6fc8ba92d489f6a82671fe98
salman9633 <salmanvaipees913@gmail.com>
2026-01-18 20:34:15 +0300

    Basics of Trait

---
+++ traits/src/main.rs
@@ -0,0 +1,34 @@
+/* Defining Trait */
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+/* Defining Struct */
+struct User {
+    name: String,
+    age: u32,
+}
+
+/* Implementing the trait with struct */
+impl Summary for User {
+    fn summarize(&self) -> String {
+        return format!("The Name is {} of {} years of old", self.name, self.age);
+    }
+}
+
+fn main() {
+    /*
+    In Rust, traits are a core feature that define shared behavior.
+    If structs define data, then traits define what that data can do.
+
+    Like interface in TS & abstract in java
+    */
+
+    let user = User {
+        name: String::from("SALMAN FARIS"),
+        age: 24,
+    };
+
+    /* using that trait function to get what we want */
+    println!("{}", user.summarize());
+}
