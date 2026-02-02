83bf1bb00b0788857778129c618e5e3c8f3d075d
salman9633 <salmanvaipees913@gmail.com>
2026-02-02 20:13:22 +0300

    divided in to sep files

---
+++ CLI_app/src/lib.rs
@@ -0,0 +1,26 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+    println!("{}", content);
+
+    Ok(())
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not Enough Arguments Passed");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
--- CLI_app/src/main.rs
+++ CLI_app/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+use CLI_app::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,32 +11,9 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = CLI_app::run(config) {
         println!("Application Error: {}", e);
         process::exit(1);
     };
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.filename)?;
-    println!("{}", content);
-
-    Ok(())
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not Enough Arguments Passed");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
