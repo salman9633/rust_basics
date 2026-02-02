b90b79144a79d9083ab0c89bf762cfc0891389f4
salman9633 <salmanvaipees913@gmail.com>
2026-02-02 20:46:33 +0300

    search with case insetive also where the boolean set with env variable

--- CLI_app/src/lib.rs
+++ CLI_app/src/lib.rs
@@ -1,15 +1,26 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub is_case_sensitive: bool,
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
-    println!("{}", content);
+    // println!("{}", content);
 
+    let result = if config.is_case_sensitive {
+        search(&config.query, &content)
+    } else {
+        case_insensitive_search(&config.query, &content)
+    };
+
+    for line in result{
+        println!("{}",line)
+    }
     Ok(())
 }
 
@@ -20,7 +31,37 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+        let is_case_sensitive = env::var("IS_CASE_SENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query,
+            filename,
+            is_case_sensitive,
+        })
     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    return results;
+}
+
+pub fn case_insensitive_search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    return results
 }
