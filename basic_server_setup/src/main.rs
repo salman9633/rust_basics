d5cdf93127111a1fa3e37c9a61004d0001e640b9
salman9633 <salmanvaipees913@gmail.com>
2026-03-08 14:53:16 +0300

    response with HTML Template

--- basic_server_setup/src/main.rs
+++ basic_server_setup/src/main.rs
@@ -1,20 +1,21 @@
+use std::fs;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    let listner =TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
     println!("Connecting to port 7878");
 
-    for stream in listner.incoming(){
-            let stream = stream.unwrap();
-            println!("{:?}",stream);
-            handle_connection(stream);
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+        println!("{:?}", stream);
+        handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream:TcpStream){
-    let mut buffer = [0;1024];
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
@@ -23,9 +24,14 @@ fn handle_connection(mut stream:TcpStream){
     //     String::from_utf8_lossy(&buffer[..])
     // );
 
-    let response = "HTTP/1.1 200 OK\r\n\r\nHello from Rust web server!";
+    let content = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        content.len(),
+        content
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
 }
