703f03116c0314999c5ce415cf87117364f1a5b2
salman9633 <salmanvaipees913@gmail.com>
2026-03-08 11:55:29 +0300

    Basic Intial Connection set up

---
+++ basic_server_setup/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listner =TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Connecting to port 7878");
+
+    for stream in listner.incoming(){
+            let stream = stream.unwrap();
+            println!("{:?}",stream);
+            handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream:TcpStream){
+    let mut buffer = [0;1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    // println!(
+    //     "request : {}",
+    //     String::from_utf8_lossy(&buffer[..])
+    // );
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+}
